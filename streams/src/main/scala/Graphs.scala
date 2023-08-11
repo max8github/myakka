9533162e9acc0644003ed426aac1a6ee513a5e6e
Ivan Krechetov <ivan.krechetov@sdx.com>
2023-08-11 10:00:12 +0200

    Generate the author handles

--- streams/src/main/scala/Graphs.scala
+++ streams/src/main/scala/Graphs.scala
@@ -1,5 +1,6 @@
 package org.example.akka.streams
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
@@ -7,28 +8,13 @@ import akka.stream.{ActorAttributes, Attributes, Supervision}
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.language.postfixOps
+import scala.util.Random
 
 object Graphs extends App {
   given system: ActorSystem = ActorSystem("g")
   given ec: ExecutionContextExecutor = system.dispatcher
 
-  final case class Author(handle: String)
-  final case class Person(first: String, last: String) {
-    override def toString: String = s"$first $last"
-  }
-
-  val input = Source(
-    Author("roland.kuhn") ::
-      Author("patrik.norwall") ::
-      Author("andrzej.ludwikowski") ::
-      Author("johan.andrén") ::
-      Author("enno.runne") ::
-      Author("peter.vlugter") ::
-      Author("faultyname") ::
-      Author("renato.cavalcanti") ::
-      Author("eduardo.pinto") ::
-      Nil)
-
+  val input = Stream.badSource()
   val sinkAuthor = Sink.foreach[Author](a => println(s"FAULTY! $a"))
 
   val transformer = Flow[Author].map(transform)
@@ -55,10 +41,42 @@ object Graphs extends App {
   }
 
   val future = input
-    .log("").withAttributes(Attributes.logLevels(onElement = Logging.WarningLevel, onFinish = Logging.InfoLevel, onFailure = Logging.DebugLevel))
+    .log("").withAttributes(Attributes.logLevels(onElement = Logging.DebugLevel, onFinish = Logging.WarningLevel, onFailure = Logging.ErrorLevel))
     .via(transformer).toMat(sink)(Keep.right)
     .withAttributes(ActorAttributes.supervisionStrategy(decider))
     .run()
 
   future.onComplete(_ => system.terminate())
+}
+
+private object Stream {
+  def badSource(): Source[Author, NotUsed] =
+    Source((Gen.nextHandlerStringSeq() ++ Seq(Gen.nextToken()) ++ Gen.nextHandlerStringSeq()).map(Author.apply))
+
+  def goodSource(): Source[Author, NotUsed] =
+    Source(Gen.nextHandlerStringSeq().map(Author.apply))
+}
+
+private object Gen {
+  def nextHandlerStringSeq(): Seq[String] =
+    (1 to 1 + Rnd.nextInt(100)).map {_ => nextToken() + "." + nextToken()}
+
+  def nextToken(): String = {
+    val length = MinTokenLength + Rnd.nextInt(MaxTokenLength - MinTokenLength + 1)
+    (" " * length).map { _ => nextLetter() }
+  }
+
+  private def nextLetter(): Char =
+    Alphabet(Rnd.nextInt(Alphabet.length))
+
+  private val Alphabet = "abcdefghijklmnopqrstuvwxyz"
+  private val Rnd = Random()
+  private val MinTokenLength = 2
+  private val MaxTokenLength = 16
+}
+
+private final case class Author(handle: String)
+
+private final case class Person(first: String, last: String) {
+  override def toString: String = s"$first $last"
 }
